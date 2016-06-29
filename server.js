627cd7477049a1192f110c4189f88b2fd9f6eae6
Brendan Strong <brendanstrong8@gmail.com>
2016-06-29 16:02:45 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', function(req, res) {
+    res.send('hi');
+});
+
+app.listen(8080, function() {
+    console.log('Server is listening on port 8080');
+});
