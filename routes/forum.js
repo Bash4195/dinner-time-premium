c2144f0df1b679df796c9190808e0a83185f6d63
Brendan Strong <brendanstrong8@gmail.com>
2016-07-06 15:31:05 -0400

    Pre-Angular

--- routes/forum.js
+++ routes/forum.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/forum', function(req, res) {
-    res.render('forum/forum');
+    res.render('forum/forum', {active: 'forum'});
 });
 
 module.exports = router;
