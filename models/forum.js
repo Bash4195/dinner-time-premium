627d3dc19243170887c0c2a911efa72a01f8595d
Brendan Strong <brendanstrong8@gmail.com>
2016-07-09 19:12:28 -0400

    Styling test on forum index page. Need to deploy for it

--- models/forum.js
+++ models/forum.js
@@ -2,7 +2,17 @@ var mongoose = require('mongoose');
 
 var forumSchema = mongoose.Schema({
     title: {type: String, required: true },
-    content: {type: String, required: true}
+    content: {type: String, required: true},
+    authour: {
+        id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        },
+        name: {type: String, required: true},
+        avatar: String,
+        profileUrl: String,
+        isOnline: Boolean
+    }
 },
     {
         timestamps: true
