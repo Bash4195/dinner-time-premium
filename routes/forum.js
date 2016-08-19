ef0ba5bee43f458cb9ec68c62edf7455fb3775aa
Brendan Strong <brendanstrong8@gmail.com>
2016-08-19 12:38:21 -0400

    Fix: Unlock post option not showing up. Add: Backend permission protection on update and delete category routes. Backend permission protection on post routes

--- routes/forum.js
+++ routes/forum.js
@@ -63,52 +63,60 @@ router.post('/api/forum', middleware.isLoggedIn, function(req, res) {
 
 // UPDATE
 router.put('/api/forum/:categoryId', middleware.isLoggedIn, function(req, res) {
-    var editedCategory = req.body;
-    if(middleware.checkIfMissing(editedCategory.title)) {
-        middleware.handleError(res, 'Category title is missing', 'Title is missing', 400);
-    } else if(middleware.checkIfMissing(editedCategory.editedBy)) {
-        middleware.handleError(res, 'Category editor is missing', 'Editor is missing', 400);
+    if(middleware.hasPermission(req.user, 'forum', 'updateCategories')) {
+        var editedCategory = req.body;
+        if(middleware.checkIfMissing(editedCategory.title)) {
+            middleware.handleError(res, 'Category title is missing', 'Title is missing', 400);
+        } else if(middleware.checkIfMissing(editedCategory.editedBy)) {
+            middleware.handleError(res, 'Category editor is missing', 'Editor is missing', 400);
+        } else {
+            editedCategory.path = '/forum/' + editedCategory.title.toLowerCase();
+            Category.findByIdAndUpdate(req.params.categoryId, editedCategory, function(err, category) {
+                if(err) {
+                    middleware.handleError(res, err.message, 'Failed to update category');
+                } else {
+                    res.status(204).end('Updated category')
+                }
+            })
+        }
     } else {
-        editedCategory.path = '/forum/' + editedCategory.title.toLowerCase();
-        Category.findByIdAndUpdate(req.params.categoryId, editedCategory, function(err, category) {
-            if(err) {
-                middleware.handleError(res, err.message, 'Failed to update category');
-            } else {
-                res.status(204).end('Updated category')
-            }
-        })
+        middleware.handleError(res, 'Unauthorized request', 'You don\'t have permission to do that', 401);
     }
 });
 
 // DELETE
 router.delete('/api/forum/:categoryId', middleware.isLoggedIn, function(req, res) {
-    Category.findById(req.params.categoryId).populate('posts').exec(function(err, category) {
-        if(err) {
-            middleware.handleError(res, err.message, 'Failed to delete category');
-        } else {
-            category.posts.forEach(function(post) {
-                post.comments.forEach(function(comment) {
-                    Comment.findByIdAndRemove(comment, function(err) {
+    if(middleware.hasPermission(req.user, 'forum', 'deleteCategories')) {
+        Category.findById(req.params.categoryId).populate('posts').exec(function(err, category) {
+            if(err) {
+                middleware.handleError(res, err.message, 'Failed to delete category');
+            } else {
+                category.posts.forEach(function(post) {
+                    post.comments.forEach(function(comment) {
+                        Comment.findByIdAndRemove(comment, function(err) {
+                            if(err) {
+                                middleware.handleError(res, err.message, 'Failed to delete the comments associated with this categories posts');
+                            }
+                        })
+                    });
+                    Post.findByIdAndRemove(post._id, function(err) {
                         if(err) {
-                            middleware.handleError(res, err.message, 'Failed to delete the comments associated with this categories posts');
+                            middleware.handleError(res, err.message, 'Failed to delete the posts associated with this category');
                         }
                     })
                 });
-                Post.findByIdAndRemove(post._id, function(err) {
+                Category.findByIdAndRemove(category._id, function(err) {
                     if(err) {
-                        middleware.handleError(res, err.message, 'Failed to delete the posts associated with this category');
+                        middleware.handleError(res, err.message, 'Failed to delete category');
+                    } else {
+                        res.status(204).end('Deleted category');
                     }
-                })
-            });
-            Category.findByIdAndRemove(category._id, function(err) {
-                if(err) {
-                    middleware.handleError(res, err.message, 'Failed to delete category');
-                } else {
-                    res.status(204).end('Deleted category');
-                }
-            });
-        }
-    })
+                });
+            }
+        })
+    } else {
+        middleware.handleError(res, 'Unauthorized request', 'You don\'t have permission to do that', 401);
+    }
 });
 
 /////////////////// Posts ////////////////////////////
@@ -152,30 +160,34 @@ router.get('/api/forum/:categoryPath', function(req, res) {
 
 // CREATE
 router.post('/api/forum/:categoryPath', middleware.isLoggedIn, function(req, res) {
-    var newPost = req.body;
-    if(newPost.title === '' || newPost.title === 'undefined') {
-        middleware.handleError(res, 'Post title is missing', 'Title is missing', 400);
-    } else if(newPost.content === '' || newPost.content === 'undefined') {
-        middleware.handleError(res, 'Post content is missing', 'Content is missing', 400);
-    } else if(newPost.authour === '' || newPost.authour === 'undefined') {
-        middleware.handleError(res, 'Post authour is missing', 'Authour is missing', 400);
+    if(middleware.hasPermission(req.user, 'forum', 'createPosts')) {
+        var newPost = req.body;
+        if(newPost.title === '' || newPost.title === 'undefined') {
+            middleware.handleError(res, 'Post title is missing', 'Title is missing', 400);
+        } else if(newPost.content === '' || newPost.content === 'undefined') {
+            middleware.handleError(res, 'Post content is missing', 'Content is missing', 400);
+        } else if(newPost.authour === '' || newPost.authour === 'undefined') {
+            middleware.handleError(res, 'Post authour is missing', 'Authour is missing', 400);
+        } else {
+            Category.findOne({path: '/forum/' + req.params.categoryPath}, function(err, category) {
+                if(err) {
+                    middleware.handleError(res, err.message, 'Failed to retrieve category to create post in');
+                } else {
+                    newPost.category = category._id;
+                    Post.create(newPost, function(err, post) {
+                        if(err) {
+                            middleware.handleError(res, err.message, 'Failed to create post in ' + category.title);
+                        } else {
+                            category.posts.push(post);
+                            category.save();
+                            res.status(201).json(post);
+                        }
+                    });
+                }
+            });
+        }
     } else {
-        Category.findOne({path: '/forum/' + req.params.categoryPath}, function(err, category) {
-            if(err) {
-                middleware.handleError(res, err.message, 'Failed to retrieve category to create post in');
-            } else {
-                newPost.category = category._id;
-                Post.create(newPost, function(err, post) {
-                    if(err) {
-                        middleware.handleError(res, err.message, 'Failed to create post in ' + category.title);
-                    } else {
-                        category.posts.push(post);
-                        category.save();
-                        res.status(201).json(post);
-                    }
-                });
-            }
-        });
+        middleware.handleError(res, 'Unauthorized request', 'You don\'t have permission to do that', 401);
     }
 });
 
@@ -194,20 +206,31 @@ router.get('/api/forum/:categoryPath/:postId', function(req, res) {
 
 // UPDATE
 router.put('/api/forum/:categoryId/:postId', middleware.isLoggedIn, function(req, res) {
-    var editedPost = req.body;
-    if(middleware.checkIfMissing(editedPost.title)) {
-        middleware.handleError(res, 'Post title is missing', 'Title is missing', 400);
-    } else if(middleware.checkIfMissing(editedPost.content)) {
-        middleware.handleError(res, 'Post content is missing', 'Content is missing', 400);
-    } else {
-        Post.findByIdAndUpdate(req.params.postId, editedPost, function(err, post) {
-            if(err) {
-                middleware.handleError(res, err.message, 'Failed to update post');
+    Post.findById(req.params.postId, function(err, post) {
+        if(err) {
+            middleware.handleError(res, err.message, 'Failed to update post');
+        } else {
+            if(req.user._id == post.authour || middleware.hasPermission(req.user, 'forum', 'updatePosts' 
+                    || req.body.locked && req.user._id == post.authour || req.body.locked && middleware.hasPermission(req.user, 'forum', 'lockPosts'))) {
+                var editedPost = req.body;
+                if(middleware.checkIfMissing(editedPost.title)) {
+                    middleware.handleError(res, 'Post title is missing', 'Title is missing', 400);
+                } else if(middleware.checkIfMissing(editedPost.content)) {
+                    middleware.handleError(res, 'Post content is missing', 'Content is missing', 400);
+                } else {
+                    Post.findByIdAndUpdate(req.params.postId, editedPost, function(err, post) {
+                        if(err) {
+                            middleware.handleError(res, err.message, 'Failed to update post');
+                        } else {
+                            res.status(204).end('Updated post')
+                        }
+                    })
+                }
             } else {
-                res.status(204).end('Updated post')
+                middleware.handleError(res, 'Unauthorized request', 'You don\'t have permission to do that', 401);
             }
-        })
-    }
+        }
+    });
 });
 
 // DELETE
@@ -216,29 +239,33 @@ router.delete('/api/forum/:categoryId/:postId', middleware.isLoggedIn, function(
         if(err) {
             middleware.handleError(res, err.message, 'Failed to delete post');
         } else {
-            Category.findById(post.category, function(err, category) {
-                if(err) {
-                    middleware.handleError(res, err.message, 'Failed to delete post');
-                } else {
-                    var postToRemove = category.posts.indexOf(post._id);
-                    category.posts.splice(postToRemove, 1);
-                    category.save();
-                }
-            });
-            post.comments.forEach(function(comment) {
-                Comment.findByIdAndRemove(comment, function(err) {
+            if(req.user._id == post.authour || middleware.hasPermission(req.user, 'forum', 'deletePosts')) {
+                Category.findById(post.category, function(err, category) {
                     if(err) {
-                        middleware.handleError(res, err.message, 'Failed to delete the comments associated with this post');
+                        middleware.handleError(res, err.message, 'Failed to delete post');
+                    } else {
+                        var postToRemove = category.posts.indexOf(post._id);
+                        category.posts.splice(postToRemove, 1);
+                        category.save();
                     }
-                })
-            });
-            Post.findByIdAndRemove(post._id, function(err) {
-                if(err) {
-                    middleware.handleError(res, err.message, 'Failed to delete post');
-                } else {
-                    res.status(204).end('Deleted post');
-                }
-            });
+                });
+                post.comments.forEach(function(comment) {
+                    Comment.findByIdAndRemove(comment, function(err) {
+                        if(err) {
+                            middleware.handleError(res, err.message, 'Failed to delete the comments associated with this post');
+                        }
+                    })
+                });
+                Post.findByIdAndRemove(post._id, function(err) {
+                    if(err) {
+                        middleware.handleError(res, err.message, 'Failed to delete post');
+                    } else {
+                        res.status(204).end('Deleted post');
+                    }
+                });
+            } else {
+                middleware.handleError(res, 'Unauthorized request', 'You don\'t have permission to do that', 401);
+            }
         }
     })
 });
