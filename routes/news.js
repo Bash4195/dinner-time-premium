3e26d1e817e6eecdc7f1344e59ff0ed68b5b1678
Brendan Strong <brendanstrong8@gmail.com>
2016-08-26 18:11:11 -0400

    Add: Update and delete back end routes for news events (Not comments). Remove: Allowing news event authour to edit or delete the news event. Only checking permissions instead

--- routes/news.js
+++ routes/news.js
@@ -55,69 +55,53 @@ router.get('/api/news/:newsId', function(req, res) {
 // EDIT - In dialog
 
 // UPDATE
-// router.put('/api/forum/:categoryId/:postId', middleware.isLoggedIn, function(req, res) {
-//     Post.findById(req.params.postId, function(err, post) {
-//         if(err) {
-//             middleware.handleError(res, err.message, 'Failed to update post');
-//         } else {
-//             if(req.user._id == post.authour || middleware.hasPermission(req.user, 'forum', 'updatePosts'
-//                     || req.body.locked && req.user._id == post.authour || req.body.locked && middleware.hasPermission(req.user, 'forum', 'lockPosts'))) {
-//                 var editedPost = req.body;
-//                 if(middleware.checkIfMissing(editedPost.title)) {
-//                     middleware.handleError(res, 'Post title is missing', 'Title is missing', 400);
-//                 } else if(middleware.checkIfMissing(editedPost.content)) {
-//                     middleware.handleError(res, 'Post content is missing', 'Content is missing', 400);
-//                 } else {
-//                     Post.findByIdAndUpdate(req.params.postId, editedPost, function(err, post) {
-//                         if(err) {
-//                             middleware.handleError(res, err.message, 'Failed to update post');
-//                         } else {
-//                             res.status(204).end('Updated post')
-//                         }
-//                     })
-//                 }
-//             } else {
-//                 middleware.handleError(res, 'Unauthorized request', 'You don\'t have permission to do that', 401);
-//             }
-//         }
-//     });
-// });
+router.put('/api/news/:newsId', middleware.isLoggedIn, middleware.isSuperAdmin, function(req, res) {
+    if(middleware.hasPermission(req.user, 'news', 'updateNews')) {
+        var editedNewsEvent = req.body;
+        if(middleware.checkIfMissing(editedNewsEvent.title)) {
+            middleware.handleError(res, 'News event title is missing', 'Title is missing', 400);
+        } else if(middleware.checkIfMissing(editedNewsEvent.content)) {
+            middleware.handleError(res, 'News event content is missing', 'Content is missing', 400);
+        } else {
+            News.findByIdAndUpdate(req.params.newsId, editedNewsEvent, function(err, newsEvent) {
+                if(err) {
+                    middleware.handleError(res, err.message, 'Failed to update news event');
+                } else {
+                    res.status(204).end('Updated news event')
+                }
+            })
+        }
+    } else {
+        middleware.handleError(res, 'Unauthorized request', 'You don\'t have permission to do that', 401);
+    }
+});
 
 // DELETE
-// router.delete('/api/forum/:categoryId/:postId', middleware.isLoggedIn, function(req, res) {
-//     Post.findById(req.params.postId, function(err, post) {
-//         if(err) {
-//             middleware.handleError(res, err.message, 'Failed to delete post');
-//         } else {
-//             if(req.user._id == post.authour || middleware.hasPermission(req.user, 'forum', 'deletePosts')) {
-//                 Category.findById(post.category, function(err, category) {
-//                     if(err) {
-//                         middleware.handleError(res, err.message, 'Failed to delete post');
-//                     } else {
-//                         var postToRemove = category.posts.indexOf(post._id);
-//                         category.posts.splice(postToRemove, 1);
-//                         category.save();
-//                     }
-//                 });
-//                 post.comments.forEach(function(comment) {
-//                     Comment.findByIdAndRemove(comment, function(err) {
-//                         if(err) {
-//                             middleware.handleError(res, err.message, 'Failed to delete the comments associated with this post');
-//                         }
-//                     })
-//                 });
-//                 Post.findByIdAndRemove(post._id, function(err) {
-//                     if(err) {
-//                         middleware.handleError(res, err.message, 'Failed to delete post');
-//                     } else {
-//                         res.status(204).end('Deleted post');
-//                     }
-//                 });
-//             } else {
-//                 middleware.handleError(res, 'Unauthorized request', 'You don\'t have permission to do that', 401);
-//             }
-//         }
-//     })
-// });
+router.delete('/api/news/:newsId', middleware.isLoggedIn, middleware.isSuperAdmin, function(req, res) {
+    if(middleware.hasPermission(req.user, 'news', 'deleteNews')) {
+        News.findById(req.params.newsId, function(err, news) {
+            if(err) {
+                middleware.handleError(res, err.message, 'Failed to delete news event');
+            } else {
+                news.comments.forEach(function(comment) {
+                    NewsComment.findByIdAndRemove(comment, function(err) {
+                        if(err) {
+                            middleware.handleError(res, err.message, 'Failed to delete the comments associated with this news event');
+                        }
+                    })
+                });
+                News.findByIdAndRemove(news._id, function(err) {
+                    if(err) {
+                        middleware.handleError(res, err.message, 'Failed to delete news event');
+                    } else {
+                        res.status(204).end('Deleted news event');
+                    }
+                });
+            }
+        });
+    } else {
+        middleware.handleError(res, 'Unauthorized request', 'You don\'t have permission to do that', 401);
+    }
+});
 
 module.exports = router;
