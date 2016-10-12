d6264e18a747518e5b3bfe00f0530a81c04b78b9
Brendan Strong <brendanstrong8@gmail.com>
2016-10-12 12:12:15 -0400

    Change: Change news and forum posts from 10,000 characters to 5,000

--- models/forumPost.js
+++ models/forumPost.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 var forumPostSchema = mongoose.Schema({
     title: { type: String, maxlength: 50, required: true },
-    content: { type: String, maxlength: 10000, required: true },
+    content: { type: String, maxlength: 5000, required: true },
     authour: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     editedBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
     editedAt: { type: Date, default: Date.now },
--- models/news.js
+++ models/news.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 var newsSchema = mongoose.Schema({
         title: { type: String, maxlength: 100, required: true },
-        content: { type: String, maxlength: 10000, required: true },
+        content: { type: String, maxlength: 5000, required: true },
         authour: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
         editedBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
         editedAt: { type: Date, default: Date.now }, 
