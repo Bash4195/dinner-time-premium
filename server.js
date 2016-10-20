4e80c41232267f30b83a563373091f91d87979e7
Brendan Strong <brendanstrong8@gmail.com>
2016-10-20 15:26:39 -0400

    Add: Syncing with gmod server users. Automatically sets users rank to whatever their rank on the server is when they login

--- server.js
+++ server.js
@@ -5,9 +5,11 @@ var bodyParser = require('body-parser');
 var session = require('express-session');
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
+var JSFtp = require('jsftp');
+var fs = require('fs');
+var convertTo = require('node-steam-converter');
 var passport = require('passport');
 var SteamStrategy = require('passport-steam').Strategy;
-var middleware = require('./middleware/index');
 
 // Route requires
 var authRoutes = require('./routes/auth');
@@ -28,6 +30,56 @@ app.use(bodyParser.json());
 
 mongoose.connect(process.env.MONGODB_URI);
 
+// FTP config
+var ftp = new JSFtp({
+    host: "dtpremium.game.nfoservers.com",
+    port: 21, // defaults to 21
+    user: "dtpremium", // defaults to "anonymous"
+    pass: "v21\\fvenu2$%yGQV@WCqcC!42G^75Ber$#G4v" // defaults to "@anonymous"
+});
+
+ftp.on('error', function(err){
+    console.log('FTP ERROR: ' + err)
+});
+
+// Sync users file on server with website users file every 10 minutes
+syncServerUsers();
+setInterval(syncServerUsers, 600000);
+
+function syncServerUsers() {
+    ftp.get('/garrysmod/data/ulib/users.txt', './gmodFTP/users.txt', function(err) {
+        if(err) {
+            console.log('ERROR: ' + err);
+            console.log('Failed to retrieve users file from DTP FTP');
+        } else {
+            // fs.readFile('./gmodFTP/users.txt', 'utf8', function(err, data) {
+            //     if (err) {
+            //         console.log('Failed to read users.txt file.');
+            //     } else {
+            //         // Replace all steamIds with Steam 64 IDs so we can check them later on
+            //         var steamIds = data.match(/"STEAM.+"/g);
+            //         steamIds.forEach(function(ele, i, steamIds) {
+            //             steamIds[i] = ele.replace(/["]+/g, '');
+            //             convertTo.steam64ID(steamIds[i], function(err, steam64ID) {
+            //                 if(err) {
+            //                     console.log('ERROR converting Steam IDs: ' + err);
+            //                 } else {
+            //                     steamIds[i] = steam64ID;
+            //                 }
+            //             });
+            //             data = data.replace(/"STEAM.+"/g, 'steamId: "' + steamIds[i] + '"');
+            //         });
+            //         fs.writeFile("./gmodFTP/users.txt", data, function(err) {
+            //             if(err) {
+            //                 return console.log(err);
+            //             }
+            //         });
+            //     }
+            // });
+        }
+    });
+}
+
 // Passport config
 passport.serializeUser(function(user, done) {
     done(null, user.id);
@@ -46,6 +98,7 @@ passport.use(new SteamStrategy({
     },
     function(identifier, profile, done) {
         var user = profile._json;
+
         var userData = {
             openIdIdentifier: identifier,
             steamId: user.steamid,
@@ -55,22 +108,58 @@ passport.use(new SteamStrategy({
             avatarMedium: user.avatarmedium,
             avatarFull: user.avatarfull,
             countryCode: user.loccountrycode,
-            onlineStatus: 'Online'
+            onlineStatus: 'Online',
+            rank: 'User'
         };
-        User.findOneAndUpdate({openIdIdentifier: identifier}, userData, {upsert: true, setDefaultsOnInsert: true}, function(err, foundOrNewUser) { // Upsert allows the object to be updated if it's found
+        
+        // Grab steam 32 ID and check users.txt file to see if it exists
+        var steam32ID;
+        convertTo.steam32ID(user.steamid, function(err, id) {
             if(err) {
-                console.log('ERROR: Failed to find and update/upsert user.');
-                console.log(err);
+                console.log('ERROR while converting user Steam ID: ' + err);
+            } else {
+                steam32ID = id;
             }
-            User.findOne({openIdIdentifier: identifier}, function(err, user) {
-                if(err) {
-                    console.log('ERROR: Failed to find user logging in.');
-                    console.log(err);
-                } else {
-                    return done(err, user);
+        });
+
+        // Set rank to their rank in the server
+        fs.readFile('./gmodFTP/users.txt', 'utf8', function(err, data) {
+            if (err) {
+                console.log('Failed to read users.txt file.');
+            } else {
+                var users = data.match(/"STEAM([\s\S]*?)"group"([\s\S]*?)}/g);
+                var steamIds = data.match(/"STEAM.+"/g);
+                // If there's a match (ie. the user logging in has a rank in the server), make sure we set their rank on the server on the site
+                if(steamIds.includes('"' + steam32ID + '"')) {
+                    var currentUserData = '';
+                    users.forEach(function(ele, i) {
+                        if(ele.includes('"' + steam32ID + '"')) {
+                            currentUserData = ele;
+                        }
+                    });
+                    currentUserData = currentUserData.match(/"group"\s".+"/);
+                    currentUserData = currentUserData[0].replace('group', '');
+                    currentUserData = currentUserData.replace(/"/g, '');
+                    currentUserData = currentUserData.replace(/\s/g, '');
+                    userData.rank = currentUserData;
                 }
+            }
+
+            User.findOneAndUpdate({openIdIdentifier: identifier}, userData, {upsert: true, setDefaultsOnInsert: true}, function(err, foundOrNewUser) { // Upsert allows the object to be updated if it's found
+                if(err) {
+                    console.log('ERROR: Failed to find and update/upsert user.');
+                    console.log(err);
+                }
+                User.findOne({openIdIdentifier: identifier}, function(err, user) {
+                    if(err) {
+                        console.log('ERROR: Failed to find user logging in.');
+                        console.log(err);
+                    } else {
+                        return done(err, user);
+                    }
+                });
             });
-        })
+        });
     }
 ));
 
